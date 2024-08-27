5a4734fd64ea50ab42c8290664f2ffa1633cedfa
ReynanddXD <165867557+ReynanddXD@users.noreply.github.com>
2024-08-27 20:05:56 +0700

    Add files via upload

---
+++ mahasiswa/Mahasiswa.java
@@ -0,0 +1,13 @@
+package mahasiswa;
+
+public class Mahasiswa {
+    void membaca(){
+        System.out.println("Mahasiswa harus rajin membaca");
+    }
+    void nyontek(){
+        System.out.println("Mahasiswa jangan sering menyontek");
+    }
+    void modifikasi(){
+        System.out.println("Mahasiswa harus banyak modfikasi");
+    }  
+}
---
+++ mahasiswa/mahasiswaberaksi.java
@@ -0,0 +1,14 @@
+
+package mahasiswa;
+
+public class mahasiswaberaksi {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mahasiswa mahasiswa1 = new Mahasiswa();
+        
+        mahasiswa1.membaca();
+        mahasiswa1.nyontek();
+        mahasiswa1.modifikasi();
+    }
+}
